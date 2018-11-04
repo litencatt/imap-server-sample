dce81c5d9528ecdb86f5e18ac5bd48ecfd01a032
Nakamura Kosuke <ncl0709@gmail.com>
2018-11-04 13:36:37 +0900

    Setup linux

---
+++ cookbooks/linux/recipe/default.rb
@@ -0,0 +1,22 @@
+%w(
+  epel-release
+  gcc
+  make
+  strace
+  sysstat
+  lsof
+  tcpdump
+  telnet
+  nc
+  traceroute
+  patch
+  openssl-devel
+  readline-devel
+  zlib-devel
+).each do |pkg|
+  package pkg
+end
+
+service 'iptables' do
+  action [:disable, :stop]
+end
