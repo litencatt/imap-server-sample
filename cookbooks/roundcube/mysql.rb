ade21eb1fc739f914f5f2ee062aabfdf93e45db4
Nakamura Kosuke <ncl0709@gmail.com>
2018-12-15 12:28:56 +0900

    Fix grant user name

--- cookbooks/roundcube/mysql.rb
+++ cookbooks/roundcube/mysql.rb
@@ -3,7 +3,7 @@ execute "grant_roundcube_privirage" do
   not_if "mysql -u root -e 'select user from mysql.user' | grep roundcubemail"
   command <<-EOL
     mysql -u root -e "CREATE DATABASE roundcubemail;"
-    mysql -u root -e "GRANT ALL PRIVILEGES ON roundcubemail.* TO vagrant@'localhost' IDENTIFIED BY 'password';"
+    mysql -u root -e "GRANT ALL PRIVILEGES ON roundcubemail.* TO roundcube@'localhost' IDENTIFIED BY 'password';"
     mysql -u root -e "FLUSH PRIVILEGES;"
   EOL
 end
